12b6a7c72a66ddbd54bcde672f5554d518be5339
Guoqiang <guoqiang_thinkpad@genhot.cn>
2023-05-21 21:48:16 +0800

    - Test GPU memory

--- llmzoo/deploy/webapp/model_memo.py
+++ llmzoo/deploy/webapp/model_memo.py
@@ -1,4 +1,4 @@
-import inference
+import llmzoo.deploy.webapp.inference as inference
 
 if __name__ == '__main__':
     model, tokenizer = inference.load_model('FreedomIntelligence/phoenix-inst-chat-7b', 'cuda', 1, None, False, False)
