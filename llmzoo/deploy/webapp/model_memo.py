4646bf4c6953a2704f7ecca4324bf234f605e70e
Guoqiang <guoqiang_thinkpad@genhot.cn>
2023-05-21 21:23:54 +0800

    - Test GPU memory

--- llmzoo/deploy/webapp/model_memo.py
+++ llmzoo/deploy/webapp/model_memo.py
@@ -1,7 +1,8 @@
 import inference
 
-model, tokenizer = inference.load_model('FreedomIntelligence/phoenix-inst-chat-7b', 'cuda', 1, None, False, False)
+if __name__ == '__main__':
+    model, tokenizer = inference.load_model('FreedomIntelligence/phoenix-inst-chat-7b', 'cuda', 1, None, False, False)
 
-inputs = tokenizer.encode("Translate to English: Je t’aime.", return_tensors="pt")
-outputs = model.generate(inputs)
-print(tokenizer.decode(outputs[0]))
+    inputs = tokenizer.encode("Translate to English: Je t’aime.", return_tensors="pt")
+    outputs = model.generate(inputs)
+    print(tokenizer.decode(outputs[0]))
