70b19a9bad8497e605f1d6934d5e5a7c8c8cea5c
Guoqiang <guoqiang_thinkpad@genhot.cn>
2023-05-26 17:42:57 +0800

    - accelerate

--- scripts/train_phoenix_7b.sh
+++ scripts/train_phoenix_7b.sh
@@ -3,28 +3,28 @@ model_max_length=2048
 data_path=/content/drive/MyDrive/bloom-training/train/data/data.json
 output_dir=checkpoints/phoenix_7b/
 
-python train.py \
---model_name_or_path ${model_name_or_path} \
---model_max_length ${model_max_length} \
---data_path ${data_path} \
---output_dir ${output_dir} \
---bf16 True \
---num_train_epochs 3 \
---per_device_train_batch_size 2 \
---per_device_eval_batch_size 2 \
---gradient_accumulation_steps 4 \
---save_strategy "steps" \
---save_steps 500 \
---evaluation_strategy "no" \
---save_total_limit 3 \
---learning_rate 2e-5 \
---weight_decay 0. \
---warmup_ratio 0.03 \
---lr_scheduler_type "cosine" \
---logging_steps 1 \
---tf32 True \
---gradient_checkpointing True \
---lora True \
---lora_r 16 \
---lora_alpha 32 \
---lora_dropout 0.05
+accelerate launch --config_file ./accelerate_config.yaml train.py \
+  --model_name_or_path ${model_name_or_path} \
+  --model_max_length ${model_max_length} \
+  --data_path ${data_path} \
+  --output_dir ${output_dir} \
+  --bf16 True \
+  --num_train_epochs 3 \
+  --per_device_train_batch_size 2 \
+  --per_device_eval_batch_size 2 \
+  --gradient_accumulation_steps 4 \
+  --save_strategy "steps" \
+  --save_steps 500 \
+  --evaluation_strategy "no" \
+  --save_total_limit 3 \
+  --learning_rate 2e-5 \
+  --weight_decay 0. \
+  --warmup_ratio 0.03 \
+  --lr_scheduler_type "cosine" \
+  --logging_steps 1 \
+  --tf32 True \
+  --gradient_checkpointing True \
+  --lora True \
+  --lora_r 16 \
+  --lora_alpha 32 \
+  --lora_dropout 0.05
